8ce2e7716b296d571d5c9d0240dd6765a35298c7
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-04-06 22:40:09 +0300

    Created User class

---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User {
+
+    // public - need access from outside users class (expand the scope)
+
+    public String name;
+    public String email;
+    public String phone;
+    public String address;
+    public String password;
+
+
+
+}
